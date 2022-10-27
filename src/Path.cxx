a46fc55bb87d7c6a946b173523a614a67daabe23
Max Kellermann <max.kellermann@gmail.com>
2022-10-27 09:22:27 +0200

    Path: add class RelativePath

--- src/Path.cxx
+++ src/Path.cxx
@@ -32,6 +32,7 @@
 
 #include "Path.hxx"
 #include "lua/Class.hxx"
+#include "lua/Value.hxx"
 #include "io/UniqueFileDescriptor.hxx"
 
 extern "C" {
@@ -52,16 +53,45 @@ public:
 		       std::string_view _path) noexcept
 		:fd(std::move(_fd)), path(_path) {}
 
+	const std::string &GetPath() const noexcept {
+		return path;
+	}
+
 	operator PathReference() const noexcept {
 		return {fd, ""};
 	}
 
 	static int ToString(lua_State *L);
+	static int Div(lua_State *L);
 };
 
-static constexpr char lua_unique_file_descriptor_class[] = "PathDescriptor";
+class RelativePath {
+	Lua::Value l_base;
+	const PathDescriptor &base;
+	std::string path;
+
+public:
+	RelativePath(lua_State *L, Lua::StackIndex base_idx,
+		     const PathDescriptor &_base,
+		     std::string_view _path) noexcept
+		:l_base(L, base_idx), base(_base), path(_path) {}
+
+	operator PathReference() const noexcept {
+		const PathReference b = base;
+		return {b.directory_fd, path.c_str()};
+	}
+
+	static int ToString(lua_State *L);
+	static int Div(lua_State *L);
+};
+
+static constexpr char lua_path_descriptor_class[] = "PathDescriptor";
 using LuaPathDescriptor = Lua::Class<PathDescriptor,
-				     lua_unique_file_descriptor_class>;
+				     lua_path_descriptor_class>;
+
+static constexpr char lua_relative_path_class[] = "RelativePath";
+using LuaRelativePath = Lua::Class<RelativePath,
+				   lua_relative_path_class>;
 
 static const auto &
 CastLuaPathDescriptor(lua_State *L, int idx)
@@ -75,16 +105,73 @@ CheckLuaPathDescriptor(lua_State *L, int idx)
 	return LuaPathDescriptor::Check(L, idx);
 }
 
+static const auto &
+CastLuaRelativePath(lua_State *L, int idx)
+{
+	return LuaRelativePath::Cast(L, idx);
+}
+
+static const auto *
+CheckLuaRelativePath(lua_State *L, int idx)
+{
+	return LuaRelativePath::Check(L, idx);
+}
+
 int
 PathDescriptor::ToString(lua_State *L)
 {
 	if (lua_gettop(L) != 1)
 		return luaL_error(L, "Invalid parameters");
 
-	[[maybe_unused]]
 	const auto &pd = CastLuaPathDescriptor(L, 1);
 
 	Lua::Push(L, pd.path);
+	return 1;
+}
+
+int
+PathDescriptor::Div(lua_State *L)
+{
+	if (lua_gettop(L) != 2)
+		return luaL_error(L, "Invalid parameters");
+
+	const auto &pd = CastLuaPathDescriptor(L, 1);
+
+	if (!lua_isstring(L, 2))
+		luaL_argerror(L, 2, "string expected");
+
+	const char *s = lua_tostring(L, 2);
+
+	LuaRelativePath::New(L, L, Lua::StackIndex{1}, pd, s);
+	return 1;
+}
+
+int
+RelativePath::ToString(lua_State *L)
+{
+	if (lua_gettop(L) != 1)
+		return luaL_error(L, "Invalid parameters");
+
+	const auto &rp = CastLuaRelativePath(L, 1);
+	Lua::Push(L, rp.base.GetPath() + "/" + rp.path);
+	return 1;
+}
+
+int
+RelativePath::Div(lua_State *L)
+{
+	if (lua_gettop(L) != 2)
+		return luaL_error(L, "Invalid parameters");
+
+	const auto &rp = CastLuaRelativePath(L, 1);
+
+	if (!lua_isstring(L, 2))
+		luaL_argerror(L, 2, "string expected");
+
+	const char *s = lua_tostring(L, 2);
+
+	LuaRelativePath::New(L, L, Lua::StackIndex{1},
+			     rp.base, s);
 	return 1;
 }
 
@@ -96,6 +183,13 @@ RegisterLuaPath(lua_State *L) noexcept
 	LuaPathDescriptor::Register(L);
 	SetTable(L, RelativeStackIndex{-1},
 		 "__tostring", PathDescriptor::ToString);
+	SetTable(L, RelativeStackIndex{-1}, "__div", PathDescriptor::Div);
+	lua_pop(L, 1);
+
+	LuaRelativePath::Register(L);
+	SetTable(L, RelativeStackIndex{-1},
+		 "__tostring", RelativePath::ToString);
+	SetTable(L, RelativeStackIndex{-1}, "__div", RelativePath::Div);
 	lua_pop(L, 1);
 }
 
@@ -113,6 +207,8 @@ GetLuaPath(lua_State *L, int idx)
 		return {FileDescriptor{AT_FDCWD}, lua_tostring(L, idx)};
 	} else if (auto *p = CheckLuaPathDescriptor(L, idx)) {
 		return *p;
+	} else if (auto *r = CheckLuaRelativePath(L, idx)) {
+		return *r;
 	} else {
 		luaL_argerror(L, idx, "path expected");
 		return {}; // unreachable
