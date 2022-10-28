1e9ab434a53542d867de4648077980aa009b5fec
Max Kellermann <max.kellermann@gmail.com>
2022-10-28 13:55:55 +0200

    examples: add Random demo

--- examples/files.lua
+++ examples/files.lua
@@ -3,4 +3,5 @@ make_directory(path/"foo/a/b")
 recursive_copy(src/"../src", path/"foo/a/b");
 
 name="world"
+r=Random:new("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|")
 copy_template(src/"template.txt", path/"with_template")
