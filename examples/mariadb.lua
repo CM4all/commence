dac55c7af03010b2ec552d5d3db96b1152b59684
Max Kellermann <max.kellermann@gmail.com>
2022-11-08 12:42:53 +0100

    lua/mariadb/Connection: use mysql_real_query() for Execute() without parameters

--- examples/mariadb.lua
+++ examples/mariadb.lua
@@ -1,4 +1,4 @@
-c = mariadb:new({host="192.168.33.6",user='foo',passwd='bar',db='xyz'})
-result = c:execute("SELECT NOW()")
+c = mariadb:new({host="192.168.33.6",user='foo',passwd='bar',db='xyz',multi_statements=true})
+result = c:execute("SELECT NOW(); SELECT 42;")
 row = result:fetch()
 print("now", row["NOW()"])
